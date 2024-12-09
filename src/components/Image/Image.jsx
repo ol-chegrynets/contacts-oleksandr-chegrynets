00d5c4d1c5fb4ea2b5fce87f4258c1a198879119
shura73 <ol.chegrynets@gmail.com>
2024-12-09 21:24:03 +0100

    avatarUrl

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,13 +1,10 @@
 import { Button } from "../Button/Button";
 import s from "./Image.module.css";
+import avatarURL from "../../assets/images/Oleksandr-Chegrynets.png";
 const Image = () => {
   return (
     <div className={s.card}>
-      <img
-        className={s.image}
-        src="../../../public/Oleksandr-Chegrynets.png"
-        alt="My Photo"
-      />
+      <img className={s.image} src={avatarURL} alt="My Photo" />
       <h2 className={s.name}>Oleksandr Chegrynets</h2>
       <Button>+ Add to Contacts</Button>
     </div>
